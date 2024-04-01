159772b1be9b3a6be648d75d6f78a6391db1b652
System administrator <for.oliver.kirkham@github.com>
2024-04-01 20:27:35 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+rm -r .build
+mkdir .build
+
+rm -r /web/*
+
+function file {
+    cp $1 /web/$1
+}
+
+file favicon.png
+file site.css
+
+function build_page {
+    gcc -E - < $1 > .build/$1.preprocessed
+    sed '/^#/d' < .build/$1.preprocessed > /web/$1
+}
+
+build_page index.html
+build_page whoami.html
+build_page small-blog.html
+build_page system.html
